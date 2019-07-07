57532bdd1182f6a232bedd27feb9f054d902b29b
Mythologos <bothwells@xavier.edu>
2019-07-07 19:23:36 -0400

    Removed one unnecessary file, tried to tweak widgets for real this time.

--- Exercise Answers/Lecture 1 & Bridge 1 - Sample Answers/bridge_tests.py
+++
@@ -1,8 +0,0 @@
-import math
-
-for i in [abs(5), abs(-5), 10**(1/2), pow(10, 1/2), 5**3, pow(5, 3), 25**(1/2),
-          math.sqrt(25), round(25.51), round(25.49), bin(10), hex(10), "4,200".isdigit(),
-          "Regal Title".istitle(), "PeasantTitle".istitle(), str(10), int(10),
-          float(10), len("string"), bool(10 % 2)]:
-    print(i)
-    print(type(i))
